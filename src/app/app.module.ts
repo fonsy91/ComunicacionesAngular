d2300c3d920c78f17ab66c73e1a05f9d288ef4d3
alfonso91 <alfonso91fp@gmail.com>
2023-09-26 18:47:44 +0200

    Modificaciones de estilos en el componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { MenuScriptService } from './services/menu-script.service';
@@ -26,7 +25,6 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     RouterModule.forRoot(routes)
   ],
   providers: [
